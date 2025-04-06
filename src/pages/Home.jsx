fef9b90be369b674bbf1dc12de70233c9346f197
Birhanu <birhanumatebe12@gmail.com>
2025-04-06 13:36:01 +0300

    home page structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Feature from '../components/Feature'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='home'>
+      <Header/>
+      <Hero/>
+      <Feature/>
+      <Banner/>
+      <Testimonial/>
+      <Footer/>
+    </section>
   )
 }
 
