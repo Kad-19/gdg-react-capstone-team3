62299ffc973c3cba68bc8450aff795e0db61347e
Birhanu <birhanumatebe12@gmail.com>
2025-04-06 14:00:16 +0300

    component reduced

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Feature from '../components/Feature'
-import Banner from '../components/Banner'
-import Testimonial from '../components/Testimonial'
-import Footer from '../components/Footer'
+
 
 const Home = () => {
   return (
     <section className='home'>
-      <Header/>
-      <Hero/>
-      <Feature/>
-      <Banner/>
-      <Testimonial/>
-      <Footer/>
+     
     </section>
   )
 }
